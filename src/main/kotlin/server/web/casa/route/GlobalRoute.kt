a751ab974da69de8e6bb3524bee281e29569c94f
ElieOko <elieoko100@gmail.com>
2025-10-06 15:02:21 +0100

    fix:refactor file

--- src/main/kotlin/server/web/casa/route/GlobalRoute.kt
+++ src/main/kotlin/server/web/casa/route/GlobalRoute.kt
@@ -1,9 +1,6 @@
 package server.web.casa.route
 
-import server.web.casa.route.auth.AuthRoute
-
 object GlobalRoute{
-//    val auth = AuthRoute()
    const val ROOT = "api"
-   const val AUTH = "/auth/"
+   const val AUTH = "auth"
 }
