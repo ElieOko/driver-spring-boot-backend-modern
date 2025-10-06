d808d94256bc455704034951a403134ef2dc0c79
ElieOko <elieoko100@gmail.com>
2025-10-06 13:35:32 +0100

    feat:add method getAllCity into controller

---
+++ src/main/kotlin/server/web/casa/app/address/infrastructure/controller/CityController.kt
@@ -0,0 +1,25 @@
+package server.web.casa.app.address.infrastructure.controller
+
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import server.web.casa.app.address.application.service.CityService
+import server.web.casa.app.address.domain.model.City
+import server.web.casa.route.address.AddressRoute
+
+const val ROUTE_CITY = AddressRoute.CITIES
+
+@RestController
+@RequestMapping(ROUTE_CITY)
+class CityController(
+   private val service : CityService
+) {
+    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun getAllCity(): ResponseEntity<Map<String, List<City>>> {
+        val data = service.findAllCity()
+        val response = mapOf("cities" to data)
+        return ResponseEntity.ok().body(response)
+    }
+}
