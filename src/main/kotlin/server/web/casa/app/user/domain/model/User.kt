46ac8ec4c69edb8557136c20fbb784e59da15949
ElieOko <elieoko100@gmail.com>
2025-10-06 15:01:48 +0100

    feat:add model user request

--- src/main/kotlin/server/web/casa/app/user/domain/model/User.kt
+++ src/main/kotlin/server/web/casa/app/user/domain/model/User.kt
@@ -1,6 +1,8 @@
 package server.web.casa.app.user.domain.model
 
+import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.FutureOrPresent
+import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Null
 import server.web.casa.app.address.domain.model.City
@@ -22,4 +24,18 @@ data class User(
     @FutureOrPresent
     @Null
     val createdStart : LocalDateTime? = null
+)
+
+data class UserRequest(
+    @NotNull
+    val username : String,
+    @NotNull
+    val password : String,
+    val typeAccountId : Long,
+    @field:NotBlank(message = "Email is required")
+    @field:Email(message = "Invalid email format")
+    val email : String? = null,
+    @field:NotBlank(message = "Phone is required")
+    val phone : String,
+    val cityId : Long
 )
