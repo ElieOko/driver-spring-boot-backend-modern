6a955540b1a970eddfdf2c4658ec3d8895c00a15
ElieOko <elieoko100@gmail.com>
2025-10-09 11:54:48 +0100

    feat:command actor

--- src/main/kotlin/server/web/casa/app/actor/application/command/CommandLineActorComponent.kt
+++ src/main/kotlin/server/web/casa/app/actor/application/command/CommandLineActorComponent.kt
@@ -2,19 +2,22 @@ package server.web.casa.app.actor.application.command
 
 import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Profile
 import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
 import server.web.casa.app.actor.infrastructure.persistence.entity.TypeCardEntity
 import server.web.casa.app.actor.infrastructure.persistence.repository.TypeCardRepository
+import server.web.casa.utils.Mode
 
 @Component
 @Order(2)
+@Profile(Mode.DEV)
 class CommandLineActorComponent(
    private val typeCardRepository: TypeCardRepository
 ) : CommandLineRunner {
     private val log = LoggerFactory.getLogger(this::class.java)
     override fun run(vararg args: String) {
-        createCard()
+//        createCard()
     }
 
     fun createCard(){
