0ad42eca49947187791628e140022dd679e6fc2f
ElieOko <elieoko100@gmail.com>
2025-10-11 12:53:11 +0100

    feat:repository app property

---
+++ src/main/kotlin/server/web/casa/app/property/infrastructure/persistence/repository/PropertyImageKitchenRepository.kt
@@ -0,0 +1,7 @@
+package server.web.casa.app.property.infrastructure.persistence.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import server.web.casa.app.property.infrastructure.persistence.entity.PropertyImageKitchenEntity
+
+interface PropertyImageKitchenRepository : JpaRepository<PropertyImageKitchenEntity, Long> {
+}
