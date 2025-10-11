7774158d32ba0bc06bd00a1c0fc235c2d80692ba
ElieOko <elieoko100@gmail.com>
2025-10-11 16:39:12 +0100

    fix:new routes app property

---
+++ src/main/kotlin/server/web/casa/route/property/PropertyRoute.kt
@@ -0,0 +1,17 @@
+package server.web.casa.route.property
+
+import server.web.casa.route.GlobalRoute
+
+object PropertyRoute {
+    const val PROPERTY = "${GlobalRoute.ROOT}/${PropertyFeatures.PROPERTY_PATH}"
+    const val PROPERTY_FAVORITE = "${GlobalRoute.ROOT}/${PropertyFeatures.PROPERTY_FAVORITE_PATH}"
+    const val PROPERTY_FEATURE = "${GlobalRoute.ROOT}/${PropertyFeatures.PROPERTY_FEATURE_PATH}\""
+    const val PROPERTY_TYPE = "${GlobalRoute.ROOT}/${PropertyFeatures.PROPERTY_TYPE_PATH}\""
+}
+
+object PropertyFeatures{
+    const val PROPERTY_PATH = "property"
+    const val PROPERTY_FAVORITE_PATH = "$PROPERTY_PATH/favorites"
+    const val PROPERTY_FEATURE_PATH = "$PROPERTY_PATH/features"
+    const val PROPERTY_TYPE_PATH = "$PROPERTY_PATH/types"
+}
