9c75892648418213bf67e02632f58004af34cccc
ElieOko <elieoko100@gmail.com>
2025-10-15 14:50:04 +0100

    fix:const district

---
+++ src/main/kotlin/server/web/casa/utils/Constante.kt
@@ -0,0 +1,8 @@
+package server.web.casa.utils
+
+object KinshasaDistrict {
+    const val LUKUNGA = "Lukunga"
+    const val MONT_AMBA = "Mont-Amba"
+    const val FUNA = "Funa"
+    const val TSHANGU = "Tshangu"
+}
